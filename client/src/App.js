fe8d8d0c355386f7c88c88e3b5c56f028ed9d1c6
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-07 15:28:04 +0530

    client- src- App.js- Add Banner.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
