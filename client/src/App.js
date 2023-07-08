b6ca0e1d12427f71a07ea763aa69234ebf7d57e6
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-08 14:31:04 +0530

    client- src- Componets- ProjectCad.js- Add project cards.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
