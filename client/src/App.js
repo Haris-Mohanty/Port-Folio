c5c421e79bf31c6da8e9bf1679fa59447f6d76da
Haris Mohanty <harismohanty8658@gmail.com>
2023-07-08 14:41:52 +0530

    client- src- Componets- Contact.js- Code fixed.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
